f16839246534d9c1cd2844626c9a9b4d32aaa436
Maria <maria.ayanyan@gmail.com>
2021-03-08 15:32:58 +0400

    Minor fixes

--- Crawler/PageLoader/test.cpp
+++ Crawler/PageLoader/test.cpp
@@ -5,7 +5,7 @@
 int main()
 {
     PageLoader loader;
-    LoadResult res = loader.load("https://github.com/mariaayanyan/SearchEngine/commit/a0148d41ae9d117c1cf3079bd3f175e4ed1bf300");
+    LoadResult res = loader.load("https://github.com/google/gumbo-parser");
     std::cout << res.getStatus() << std::endl << std::endl;
     std::cout << *res.getBody().get() << std::endl;
 }
