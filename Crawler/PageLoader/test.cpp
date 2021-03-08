02881704078a165f2e2a40bea321731156440940
Maria <maria.ayanyan@gmail.com>
2021-03-08 15:31:21 +0400

    Minor fixes

--- Crawler/PageLoader/test.cpp
+++ Crawler/PageLoader/test.cpp
@@ -5,7 +5,7 @@
 int main()
 {
     PageLoader loader;
-    LoadResult res = loader.load("https://fuck_you");
+    LoadResult res = loader.load("https://github.com/mariaayanyan/SearchEngine/commit/a0148d41ae9d117c1cf3079bd3f175e4ed1bf300");
     std::cout << res.getStatus() << std::endl << std::endl;
     std::cout << *res.getBody().get() << std::endl;
 }
